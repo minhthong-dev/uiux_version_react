ae5738970b7b947cc1f2fbc9eec1c5b9c7fff68e
ssamkkura <tuantuntun03@gmail.com>
2026-04-08 21:38:20 +0700

    fix: add author token

--- client_fe/react/src/api/walletApi.js
+++ client_fe/react/src/api/walletApi.js
@@ -3,7 +3,11 @@ import axiosClient from "./axiosClient";
 const walletApi = {
     getAllWallets: async () => {
         try {
-            const response = await axiosClient.get('/wallets');
+            const response = await axiosClient.get('/wallets', {
+                headers: {
+                    "Authorization": `Bearer ${manageToken.getToken()}`
+                }
+            });
             console.log(response)
             return response.data;
         } catch (error) {
@@ -13,7 +17,11 @@ const walletApi = {
     },
     getWalletById: async (id) => {
         try {
-            const response = await axiosClient.get(`/wallets/${id}`);
+            const response = await axiosClient.get(`/wallets/${id}`, {
+                headers: {
+                    "Authorization": `Bearer ${manageToken.getToken()}`
+                }
+            });
             return response.data;
         } catch (error) {
             console.error("Error in getWalletById:", error);
